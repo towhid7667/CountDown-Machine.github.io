b93ddfaadbc2c55cc6bf0da513629994bee13c66
towhid7667 <94273983+towhid7667@users.noreply.github.com>
2022-04-27 02:54:07 +0600

    Year has been changed!

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minEl  = document.getElementById("min");
 const secondEl= document.getElementById("second");
 
-const newYears = "1 Jan 2022";
+const newYears = "1 Jan 2023";
 
 
 
